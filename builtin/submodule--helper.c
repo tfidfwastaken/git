be83ba7fdb22ca50ad46b899ca0a50376253ed26
Atharva Raykar <raykar.ath@gmail.com>
2021-07-20 17:11:39 +0530

    submodule--helper: introduce get_default_remote_submodule()
    Before 8ef1d2b549 (submodule--helper: get remote names from any
    repository, 2021-07-20), it was not possible to directly retrieve a
    submodule's remote name within the same process, because
    `get_default_remote()` used only knew about the current repository.
    
    Now that we have `repo_get_default_remote()`, we no longer have to start
    a subprocess that called `submodule--helper get-default-remote` from
    within the submodule directory.
    
    Let's make a function called `get_default_remote_submodule()` which
    takes a submodule path, and returns the default remote for that
    submodule, all within the same process.
    
    We can now use this function to save an unnecessary subprocess spawn in
    `sync_submodule()`, and also in the next patch, which will require this
    functionality.
    
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by: Shourya Shukla <periperidip@gmail.com>
    Signed-off-by: Atharva Raykar <raykar.ath@gmail.com>

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -54,6 +54,19 @@ static char *repo_get_default_remote(struct repository *repo, const char *refnam
 	return ret;
 }
 
+static char *get_default_remote_submodule(const char *module_path)
+{
+	const char *refname;
+	const struct submodule *sub;
+	struct repository subrepo;
+
+	refname = refs_resolve_ref_unsafe(get_submodule_ref_store(module_path),
+					  "HEAD", 0, NULL, NULL);
+	sub = submodule_from_path(the_repository, null_oid(), module_path);
+	repo_submodule_init(&subrepo, the_repository, sub);
+	return repo_get_default_remote(&subrepo, refname);
+}
+
 static char *get_default_remote(void)
 {
 	const char *refname = resolve_ref_unsafe("HEAD", 0, NULL, NULL);
@@ -1374,7 +1387,6 @@ static void sync_submodule(const char *path, const char *prefix,
 	char *remote_key = NULL;
 	char *sub_origin_url, *super_config_url, *displaypath;
 	struct strbuf sb = STRBUF_INIT;
-	struct child_process cp = CHILD_PROCESS_INIT;
 	char *sub_config_path = NULL;
 
 	if (!is_submodule_active(the_repository, path))
@@ -1423,14 +1435,9 @@ static void sync_submodule(const char *path, const char *prefix,
 	if (!is_submodule_populated_gently(path, NULL))
 		goto cleanup;
 
-	prepare_submodule_repo_env(&cp.env_array);
-	cp.git_cmd = 1;
-	cp.dir = path;
-	strvec_pushl(&cp.args, "submodule--helper",
-		     "print-default-remote", NULL);
-
 	strbuf_reset(&sb);
-	if (capture_command(&cp, &sb, 0))
+	strbuf_addstr(&sb, get_default_remote_submodule(path));
+	if (!sb.buf)
 		die(_("failed to get the default remote for submodule '%s'"),
 		      path);
 
