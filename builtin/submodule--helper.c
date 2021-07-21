a7100a59d44a8dd95c60aa096235b3048ffcd8ac
Atharva Raykar <raykar.ath@gmail.com>
2021-07-21 13:55:40 +0530

    submodule--helper: remove update-module-mode subcommand
    This subcommand was once useful for 'submodule update', but now that we
    have converted the shell code to C, it is no longer used.
    
    Signed-off-by: Atharva Raykar <raykar.ath@gmail.com>
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by: Shourya Shukla <periperidip@gmail.com>

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -1977,29 +1977,6 @@ static void determine_submodule_update_strategy(struct repository *r,
 	free(key);
 }
 
-static int module_update_module_mode(int argc, const char **argv, const char *prefix)
-{
-	const char *path, *update = NULL;
-	int just_cloned;
-	struct submodule_update_strategy update_strategy = { .type = SM_UPDATE_CHECKOUT };
-
-	if (argc < 3 || argc > 4)
-		die("submodule--helper update-module-clone expects <just-cloned> <path> [<update>]");
-
-	just_cloned = git_config_int("just_cloned", argv[1]);
-	path = argv[2];
-
-	if (argc == 4)
-		update = argv[3];
-
-	determine_submodule_update_strategy(the_repository,
-					    just_cloned, path, update,
-					    &update_strategy);
-	fputs(submodule_strategy_to_string(&update_strategy), stdout);
-
-	return 0;
-}
-
 struct update_clone_data {
 	const struct submodule *sub;
 	struct object_id oid;
@@ -3265,7 +3242,6 @@ static struct cmd_struct commands[] = {
 	{"name", module_name, 0},
 	{"clone", module_clone, 0},
 	{"update", module_update, 0},
-	{"update-module-mode", module_update_module_mode, 0},
 	{"run-update-procedure", run_update_procedure, 0},
 	{"ensure-core-worktree", ensure_core_worktree, 0},
 	{"relative-path", resolve_relative_path, 0},
